088670da8a5b0dc0017f6580fa47c3b046122ebe
Timo Esser <t.g.esser@student.tudelft.nl>
2024-02-27 16:20:37 +0100

    inputs

---
+++ python/propulsion/inputs.py
@@ -0,0 +1,12 @@
+
+
+class First_stage_requirements:
+    def __init__(self):
+
+        Thrust = 19.97e6    # Newton ; Derived from A64 (wiki); =Fz
+        Fx = 10000   #TBR
+        Fy = 10000   #TBR
+        Mx,My,Mz = 0,0,0
+
+        time_burn_1st = 100 # seconds, just a guess
+        
