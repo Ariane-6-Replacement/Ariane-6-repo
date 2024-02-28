133e5691dc37f2821ae743865e506e050351749f
Dries <dries.borstlap@gmail.com>
2024-02-28 09:12:28 +0100

    more inputs propulsion

--- propulsion/inputs.py
+++ propulsion/inputs.py
@@ -10,3 +10,22 @@ class FirstStageRequirements:
 
         time_burn_1st = 100 # seconds, just a guess
         
+
+class Engine_specs:
+    def __init__(self):
+        Isp = 347 #s (SHOULD BE CHECKED!)
+
+
+class Fuel_specs:
+    def __init__(self):
+        
+        rho_LOX = 1340 #kg/m3 liquid oxygen
+        # rho_RP1 = 860  #kg/m3 RP1 (kerosen)
+        rho_LM = 460  #kg/m3 liquid methane  # should be checked , at which temperature?
+        mix_rat = 3.8 # kg lox per kg meth
+
+
+        #Raptor Mixture Ratio: 3.8 kg LOX to 1kg Methane. [Source](https://en.wikipedia.org/wiki/Raptor_(rocket_engine)).
+
+
+
