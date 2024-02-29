c987bea2d86b70baa1686579bce6c5a9e4ccbd69
jalonso <63980841+javialonso05@users.noreply.github.com>
2024-02-29 11:07:50 +0100

    Linearization updated

--- python/control/linearizer.py
+++ python/control/linearizer.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.interpolate import CubicSpline
+from scipy.interpolate import CubicSpline, griddata
 
 
 def moment_eq(thrust, thrust_offset, moment_arm, mass_MOI):
@@ -31,45 +31,53 @@ Assumptions:
 # Define boundaries
 thrust_offset_max = np.radians(15)
 thrust_offset_min = np.radians(-15)
-a_t = np.linspace(thrust_offset_min, thrust_offset_max, 100)
+a_t = np.linspace(thrust_offset_min, thrust_offset_max, 180)
 
 # Initial conditions
 M = 900e3
 g = 9.81
 T = 5 * M * g
-flight_path = np.linspace(np.radians(5), np.radians(85), 9)
+flight_path = np.linspace(0, np.pi/2, 180)
 
 
-# Linearize the system
-ax = {}
-az = {}
-ax_at = {}
-az_at = {}
-for angle in flight_path:
-    acceleration_x, acceleration_z = force_eq(T, M, g, angle, a_t)
-    ax[angle] = acceleration_x
-    az[angle] = acceleration_z
+### Linearize the system ###
+a_grid, gamma_grid = np.meshgrid(a_t, flight_path)
+ax = 1 / M * T * np.cos(gamma_grid + a_grid)
+az = 1 / M * T * np.sin(gamma_grid + a_grid) - g
 
-    axAt = CubicSpline(a_t, acceleration_x)  # Acceleration in x given a thrust offset
-    azAt = CubicSpline(a_t, acceleration_z)  # Acceleration in z given a thrust offset
-    ax_at[angle] = axAt
-    az_at[angle] = azAt
+# linearized_system_x = griddata((a_grid.flatten(), gamma_grid.flatten()), ax.flatten(), (a_grid, gamma_grid), method='cubic')
+# linearized_system_z = griddata((a_grid.flatten(), gamma_grid.flatten()), az.flatten(), (a_grid, gamma_grid), method='cubic')
 
 if __name__ == '__main__':
-    for i in range(len(flight_path)):
-        plt.plot(np.degrees(a_t), ax[flight_path[i]], label=f'{np.round(np.degrees(flight_path[i]))}')
+    plt.imshow(ax, cmap='seismic', vmax=np.max(np.abs(ax)), vmin=-np.max(np.abs(ax)))
+    # Adapt x axis
+    locs, labels = plt.xticks()
+    labs = np.linspace(np.round(np.degrees(np.min(a_t))), np.round(np.degrees(np.max(a_t))), len(locs), dtype=int)
+    plt.xticks(np.linspace(0, 180, len(locs), dtype=int), labs)
+    # Adapt y axis
+    locs, labels = plt.yticks()
+    labs = np.linspace(np.round(np.degrees(np.min(flight_path))), np.round(np.degrees(np.max(flight_path))), len(locs), dtype=int)
+    plt.yticks(np.linspace(0, 180, len(locs), dtype=int), labs)
     plt.xlabel('Thrust offset [deg]')
-    plt.ylabel(r'Acceleration [m/s$^2$]')
-    plt.legend()
-    plt.title(r'Effect of thrust offset ($\pm$15 deg) on x-acceleration')
+    plt.ylabel('Flight path angle [deg]')
+    plt.colorbar()
+    plt.title(r'Horizontal acceleration [m/s$^2$]')
     plt.savefig('Effect of thrust offset on x-acceleration.pdf')
     plt.show()
 
-    for i in range(len(flight_path)):
-        plt.plot(np.degrees(a_t), az[flight_path[i]], label=f'{np.round(np.degrees(flight_path[i]))}')
+    plt.imshow(az, cmap='seismic', aspect='equal', vmax=np.max(np.abs(az)), vmin=-np.max(np.abs(az)))
+    # Adapt x axis
+    locs, labels = plt.xticks()
+    labs = np.linspace(np.round(np.degrees(np.min(a_t))), np.round(np.degrees(np.max(a_t))), len(locs), dtype=int)
+    plt.xticks(np.linspace(0, 180, len(locs), dtype=int), labs)
+    # Adapt y axis
+    locs, labels = plt.yticks()
+    labs = np.linspace(np.round(np.degrees(np.min(flight_path))), np.round(np.degrees(np.max(flight_path))), len(locs),
+                       dtype=int)
+    plt.yticks(np.linspace(0, 180, len(locs), dtype=int), labs)
     plt.xlabel('Thrust offset [deg]')
-    plt.ylabel(r'Acceleration [m/s$^2$]')
-    plt.legend()
-    plt.title(r'Effect of thrust offset ($\pm$15 deg) on z-acceleration')
+    plt.ylabel('Flight path angle [deg]')
+    plt.colorbar()
+    plt.title(r'Vertical acceleration [m/s$^2$]')
     plt.savefig('Effect of thrust offset on z-acceleration.pdf')
     plt.show()
