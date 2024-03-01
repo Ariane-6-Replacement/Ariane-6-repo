ce8f3ba9598b9fb800c0f2e60a44de283148afa4
mkwapien <m.t.kwapien@student.tudelft.nl>
2024-03-01 09:19:33 +0100

    Add thickness calcs

--- python/structure/Components/cylinder_class.py
+++ python/structure/Components/cylinder_class.py
@@ -1,12 +1,16 @@
 import geometry
+from NASA_SP8007 import pressure_loading, buckling, buckling_coeff, bending, axial_stress
+from Constants import FOSY, FOSU 
 class Cylinder:
     def __init__(self,
                  outer_radius: float,
-                 thickness: float,
-                 height: float,
                  material: dict,
-                 stiffening_elastic_constants: tuple,
-                 stiffening_volume: float):
+                 pressure: float,
+                 thrust: float,
+                #  stiffening_elastic_constants: tuple,
+                 height:float,
+                #  stiffening_volume: float
+                 ):
         """
         Cylinder object, containing all relevant parameters. Cylinder coordinate system is defined with the origin at the
         center of the bottom edge. The z_c axis moves along the cylinder's axis in the direction from aft to forward.
@@ -18,40 +22,59 @@ class Cylinder:
 
         # These are the parameters that are passed to the class:
         self.outer_radius = outer_radius
-        self.thickness = thickness
-        self.height = height
+        self.thickness = pressure
         self.material = material
-        self.stiffening_elastic_constants = stiffening_elastic_constants
-        self.stiffening_volume = stiffening_volume
+        self.height = height
+        self.thrust =thrust
+        # self.stiffening_elastic_constants = stiffening_elastic_constants
+        # self.stiffening_volume = stiffening_volume
         self.parent_configuration = None
 
-        if not self.is_thin_walled: raise ValueError(f"Cylinder is not thin walled (OD: {self.outer_diameter}, H: {self.height}), t: {self.thickness})")
+
+
+    
+    @property
+    def section_Ixx(self) -> float:
+        return geometry.cylindrical_shell_I(self.outer_radius, self.thickness)
+
 
     @property
-    def inner_radius(self) -> float:
-        return self.outer_radius - self.thickness
-
-    @property
-    def inner_diameter(self) -> float:
-        return 2 * self.inner_radius
-
-    @property
-    def outer_diameter(self) -> float:
-        return 2 * self.outer_radius
-
-    @property
+    def thickness(self) -> float:
+        #Calcualte Basic Thickness:
+        t_p= pressure_loading.t_hoop_stress(self.material['yield_stress'], self.outer_radius, FOSY, self.pressure)
+        #Caclculate Axial Stress Thickness:
+        t_a = axial_stress.t_axial(self.material['yield_stress'], self.outer_radius, FOSY, self.thrust)
+        #Calcualte critical Buckling stress
+        if t_a>t_p:
+            t=t_a
+        else:
+            t=t_p
+        s_buckling = buckling.critical_cylinder_buckling(self.pressure, self.outer_radius,
+                                                         self.thickness, self.height, 
+                                                   self.material['youngs_modulus'],self.material['poisson_ratio'])
+        
+        while s_buckling/FOSY >= self.material['yield_stress']:
+            t+=0.001
+            s_buckling = buckling.critical_cylinder_buckling(self.pressure, self.outer_radius,
+                                                         self.thickness, self.height, 
+                                                   self.material['youngs_modulus'],self.material['poisson_ratio'])
+        s_bending = bending.critical_cylinder_bending(self.outer_radius, t ,self.pressure, self.material['youngs_modulus'],
+                                           self.material['poisson_ratio'], self.section_Ixx)
+        #NOTE Fact check this assumption 
+        while s_bending/FOSY >= self.material['yield_stress']:
+            t+=0.001
+            s_bending = bending.critical_cylinder_bending(self.outer_radius, t 
+            ,self.pressure, self.material['youngs_modulus'],self.material['poisson_ratio'], self.section_Ixx)
+        return t
+    
     def inner_volume(self) -> float:
-        return geometry.cylinder_V(self.inner_radius, self.height)
+        return geometry.cylinder_V(self.outer_radius-self.thickness, self.height)
 
     @property
     def outer_volume(self) -> float:
         return geometry.cylinder_V(self.outer_radius, self.height)
 
     @property
-    def section_Ixx(self) -> float:
-        return geometry.cylindrical_shell_I(self.outer_radius, self.thickness)
-
-    @property
     def section_Iyy(self) -> float:
         return self.section_Ixx
 
@@ -60,38 +83,37 @@ class Cylinder:
         material_volume = self.outer_volume - self.inner_volume
         return self.material["density"] * material_volume
 
-    @property
-    def is_thin_walled(self) -> bool:
-        return (self.thickness < 0.1 * self.outer_diameter) and (self.thickness < 0.1 * self.height)
 
     @property
     def sectional_area(self) -> float:
         return geometry.cylindrical_shell_A(self.outer_radius, self.thickness)
     
-    @property
-    def stiffening_elastic_constants(self):
-        return self._stiffening_elastic_constants
 
-    @stiffening_elastic_constants.setter
-    def stiffening_elastic_constants(self, value):
-        self._stiffening_elastic_constants = value
+
+    # @property
+    # def stiffening_elastic_constants(self):
+    #     return self._stiffening_elastic_constants
+
+    # @stiffening_elastic_constants.setter
+    # def stiffening_elastic_constants(self, value):
+    #     self._stiffening_elastic_constants = value
 
     
-     @property
-    def section_Iyy(self) -> float:
-        # TODO: Adjust for stiffeners
-        return self.section_Ixx
+    #  @property
+    # def section_Iyy(self) -> float:
+    #     # TODO: Adjust for stiffeners
+    #     return self.section_Ixx
 
-    @property
-    def mass(self) -> float:
-        # TODO: Adjust for stiffeners
-        material_volume = self.outer_volume - self.inner_volume
-        material_volume += self.stiffening_volume
-        return self.material["density"] * material_volume
+    # @property
+    # def mass(self) -> float:
+    #     # TODO: Adjust for stiffeners
+    #     material_volume = self.outer_volume - self.inner_volume
+    #     material_volume += self.stiffening_volume
+    #     return self.material["density"] * material_volume
 
-    @property
-    def sectional_area(self) -> float:
-        # TODO: Adjust for stiffeners
-        return geometry.cylindrical_shell_A(self.outer_radius, self.thickness)
+    # @property
+    # def sectional_area(self) -> float:
+    #     # TODO: Adjust for stiffeners
+    #     return geometry.cylindrical_shell_A(self.outer_radius, self.thickness)
 
 
