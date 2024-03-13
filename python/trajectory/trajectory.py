d2a6ac564ed19f5b01c45793651116647d0674b8
Adourleijn <a.dourleijn@gmail.com>
2024-03-13 12:45:16 +0100

    Added framework function

--- python/trajectory/trajectory.py
+++ python/trajectory/trajectory.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 class Trajectory():
-    def __init__(self):
+    def __init__(self, orbit, payload, cd):
         data = [['Standard Geostationary Transfer Orbit', 'GTO', 6, 250, 35786, 178, 11500],
                 ['Geostationary Transfer Orbit + (GTO+) and Medium Transfer Orbit (MTO)', 'GTO+ MTO', 6, 2200, 35486, 'none', 10700],
                 ['Sub Geostationary Transfer Orbits', 'sGTO', 6, 250, 22500, 'none', 12950],
@@ -12,3 +12,7 @@ class Trajectory():
                 ['Polar Orbit', 'POL', 90, 900, 900, 'none', 15400],
                 ['Low Earth Orbit', 'LEO', 6, 250, 1200, 21650]]
         orbits_list = pd.DataFrame(data, columns=['name', 'abbreviation', 'inclination [deg]', 'altitude of perigee [km]', ' altitude of apogee [km]', 'argument of perigee [deg]', 'payload weight [kg]'])
+    def thrust_burntime(self, mass, dv):
+        thrust = 19e6
+        burntime = 100 #placeholders
+        return thrust, burntime
