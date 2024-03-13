7171912b3a62d516d8b4baad77ce5c8b18144941
Dries Borstlap <dborstlap@Driess-MacBook-Air.local>
2024-03-13 11:24:05 +0100

    volume_mass_calculater cleaned up

--- python/propulsion/volume_mass_calculator.py
+++ python/propulsion/volume_mass_calculator.py
@@ -39,29 +39,24 @@ def calculate_propellant_mass_flow_rate(thrust, Isp):
   mass_flow = thrust / (Isp*g0)
   return mass_flow
 
-
-# (checked)
+# mass flow rate
 mass_flow = calculate_propellant_mass_flow_rate(thrust, Isp)
-total_mass = mass_flow * burn_time
 
-# (checked)
-mass_ox = OF_ratio / (1+OF_ratio) * total_mass
-mass_fuel = 1 / (1+OF_ratio) * total_mass
+# mass calculations
+mass_total = mass_flow * burn_time
+mass_ox = OF_ratio / (1+OF_ratio) * mass_total
+mass_fuel = 1 / (1+OF_ratio) * mass_total
 
-# (checked)
+# volume calculations
 volume_ox = mass_ox / rho_ox
 volume_fuel = mass_fuel / rho_fuel
-total_volume = volume_ox + volume_fuel
+volume_total = volume_ox + volume_fuel
 
-
+# return/print whatever you want
 print("mass flow:",mass_flow)
 print("mass_ox:",mass_ox,"mass_fuel:",mass_fuel,"Total mass:",mass_ox+mass_fuel)
-print("Volume_ox:",volume_ox,"Volume_fuel:",volume_fuel,"Total Volume",total_volume)
+print("Volume_ox:",volume_ox,"Volume_fuel:",volume_fuel,"Total Volume",volume_total)
 
-
-# IT WORKS :-) !!!
-
-print(rho_fuel, rho_ox)
 
 
 
