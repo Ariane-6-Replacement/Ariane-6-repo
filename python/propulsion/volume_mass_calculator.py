8944b0670d8df844ca444cb287fe7ca5caff428e
Dries Borstlap <dborstlap@Driess-MacBook-Air.local>
2024-03-13 10:47:08 +0100

    density

--- python/propulsion/volume_mass_calculator.py
+++ python/propulsion/volume_mass_calculator.py
@@ -1,15 +1,21 @@
+# IMPORTS
 from inputs import FirstStageRequirements, Prometheus, Fuel
-import sys,os
 
-sys.path.append(os.path.abspath('../structure'))
-print(sys.path)
-from python.structure import structure
-
+# INPUTS
 engine = Prometheus()
 first_stage = FirstStageRequirements()
 fuel = Fuel()
 
+g0 = 9.80665
+thrust = first_stage.Thrust
+Isp = engine.Isp
+burn_time = first_stage.time_burn_1st
+OF_ratio = engine.OF_ratio
+rho_ox = fuel.rho_LOX
+rho_fuel = fuel.rho_LM
 
+
+# DEFENITIONS
 def calculate_average_fuel_density(fuel_ratio, oxidizer_density, propellant_density):
   """
   Calculates the average fuel density based on the fuel ratio, oxidizer density, and propellant density.
@@ -28,41 +34,33 @@ def calculate_average_fuel_density(fuel_ratio, oxidizer_density, propellant_dens
   return average_density
 
 
-mass_flow = first_stage.Thrust / (engine.Isp*9.80665)
-burn_time = first_stage.time_burn_1st
+def calculate_propellant_mass_flow_rate(thrust, Isp):
+  # calculate mass flow based on Isp equation
+  mass_flow = thrust / (Isp*g0)
+  return mass_flow
 
-Total_mass = mass_flow * burn_time
 
-OF_ratio = engine.OF_ratio
-mass_ox = OF_ratio / (1+OF_ratio) * Total_mass
-mass_fuel = 1 / (1+OF_ratio) * Total_mass
-rho_ox = fuel.rho_LOX
-rho_fuel = fuel.rho_LM
+# (checked)
+mass_flow = calculate_propellant_mass_flow_rate(thrust, Isp)
+total_mass = mass_flow * burn_time
+
+# (checked)
+mass_ox = OF_ratio / (1+OF_ratio) * total_mass
+mass_fuel = 1 / (1+OF_ratio) * total_mass
+
+# (checked)
 volume_ox = mass_ox / rho_ox
 volume_fuel = mass_fuel / rho_fuel
+total_volume = volume_ox + volume_fuel
 
-Total_volume = volume_ox + volume_fuel
+
 print("mass flow:",mass_flow)
 print("mass_ox:",mass_ox,"mass_fuel:",mass_fuel,"Total mass:",mass_ox+mass_fuel)
-print("Volume_ox:",volume_ox,"Volume_fuel:",volume_fuel,"Total Volume",Total_volume)
+print("Volume_ox:",volume_ox,"Volume_fuel:",volume_fuel,"Total Volume",total_volume)
 
 
+# IT WORKS :-) !!!
 
-# VERIFY THAT IT WORKS!!!
-
-
-
-# average_density = calculate_average_fuel_density(OF_ratio, fuel.rho_LOX, fuel.rho_LM)
-# Total_volume = Total_mass/average_density
-# print(Total_volume)
-
-
-# import stuff from structures
-
-# for pressure
-pressure = 7e5
-structure1 = structure.Structure(6,pressure,"6082",volume_ox,mass_ox,pressure, volume_fuel,mass_fuel, first_stage.Thrust,"6082")
-print("pressure:",pressure,"mass1:", structure1.mass_total, "height:",structure1.height_total)
 
 
 
