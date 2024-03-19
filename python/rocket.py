d10ba8e3c22cb6483b87e82ff23e403ac6322c0d
Adourleijn <a.dourleijn@gmail.com>
2024-03-19 16:17:20 +0100

    Integration

--- python/rocket.py
+++ python/rocket.py
@@ -28,6 +28,9 @@ class Rocket():
         self.OF_ratio = float(input_dict["OF_ratio"].get())
         dv1 = float(input_dict["dv_split"].get())
         self.dv_split = [dv1, 1-dv1]
+        self.mf1 = float(input_dict["mf1"].get())
+        self.mf2 = float(input_dict["mf2"].get())
+        self.isp2 = float(input_dict["ISP2"].get())
         self.root = None
         print(f"Engine: {self.engine}")
         print(f"Delta V: {self.dv} m/s")
@@ -42,26 +45,25 @@ class Rocket():
 
         #self.aerodynamics = Aerodynamics()
         #self.control = Control()
-        self.propulsion = Propulsion(self.engine)
+        self.propulsion = Propulsion(self.engine, self.OF_ratio)
         self.structure = Structure(self.diameter/2, self.material_tank, self.pressure_ox, self.pressure_fuel, self.material_misc)
         self.trajectory = Trajectory(self.orbit, self.payload, self.cd)
 
      
     def mass_estimation(self):
-        self.mass_s = 100000 # kg
-        self.mass_p = 600000 #kg
-        self.mass = self.mass_s + self.mass_p
         mass_estimator = MassCalculator()
-        wet_masses = mass_estimator.get_wet_masses(self.dv, self.dv_split, inert_mass_fractions, self.propulsion.Isp, self.payload)
-        prop_masses = mass_estimator.get_propellant_masses(wet_masses, inert_mass_fractions)
-        dry_masses = mass_estimator.get_dry_masses(wet_masses, inert_mass_fractions)
+        wet_masses = mass_estimator.get_wet_masses(self.dv, self.dv_split, [self.mf1,self.mf2],
+                                                   [self.propulsion.Isp, self.isp2], self.payload)
+        self.mass_p = mass_estimator.get_propellant_masses(wet_masses, [self.mf1,self.mf2])
+        self.mass_s = mass_estimator.get_dry_masses(wet_masses, [self.mf1,self.mf2])
+        self.mass = self.mass_s + self.mass_p
     def cost_estimator(self):
         return self.mass * 25.00 #placeholder
     def iterate(self):
 
         self.thrust, self.burntime = self.trajectory.thrust_burntime(self.mass,  self.dv)
         self.mass_e, self.mass_fuel, self.mass_ox, self.volume_fuel, self.volume_ox, self.engine_number = (
-            self.propulsion.mass_volume(self.thrust, self.burntime, self.OF_ratio))
+            self.propulsion.mass_volume(self.thrust, self.burntime))
         self.mass_p = self.mass_ox + self.mass_fuel
         self.structure.calc(self.volume_ox, self.mass_ox, self.volume_fuel, self.mass_fuel, self.thrust)
         self.mass_t = self.structure.mass_total_tank
@@ -137,38 +139,28 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("Input Screen")
     input_dict = {}
+    custom_font = ('Helvetica', 12, 'bold')
     i = 0
-    ttk.Label(root, text="Engine:").grid(column=0, row=i)
-    input_dict["engine"] = tk.StringVar()
-    engine_options = ['Prometheus']
-    ttk.Combobox(root, textvariable=input_dict["engine"], values=engine_options, state="readonly").grid(column=1, row=i)
+    ttk.Label(root, text="General properties", font=custom_font).grid(column=1, row=i)
 
-    input_dict["engine"].set("Prometheus")  # default value
     i += 1
     # Delta V
     ttk.Label(root, text="Delta V total (m/s):").grid(column=0, row=i)
     input_dict["dv"] = tk.StringVar(value='3000')
     ttk.Entry(root, textvariable=input_dict["dv"]).grid(column=1, row=i)
-
-    # Boostback
-    i += 1
-    ttk.Label(root, text="Boostback:").grid(column=0, row=i)
-    input_dict["boostback"] = tk.BooleanVar(value=False)
-    ttk.Checkbutton(root, variable=input_dict["boostback"]).grid(column=1, row=i)
-
     # Orbit
     i += 1
     ttk.Label(root, text="Orbit:").grid(column=0, row=i)
     input_dict["orbit"] = tk.StringVar()
     orbit_options = ['LEO', 'MEO', 'GEO', 'HEO']
-    ttk.Combobox(root, textvariable=input_dict["orbit"], values=orbit_options, state="readonly").grid(column=1, row=i)
+    ttk.Combobox(root, textvariable=input_dict["orbit"], values=orbit_options, state="readonly", width=17).grid(column=1, row=i)
     input_dict["orbit"].set("LEO")  # default value
 
     # Payload
     i += 1
     ttk.Label(root, text="Payload (kg):").grid(column=0, row=i)
-    input_dict["payload"]= tk.StringVar(value='20000')
-    ttk.Entry(root, textvariable=input_dict["payload"]).grid(column=1, row=i)
+    input_dict["payload"] = tk.StringVar(value='20000')
+    ttk.Entry(root, textvariable=input_dict["payload"], width=20).grid(column=1, row=i)
 
     # Drag Coefficient
     i += 1
@@ -176,12 +168,60 @@ if __name__ == "__main__":
     input_dict["cd"] = tk.StringVar(value='0.2')
     ttk.Entry(root, textvariable=input_dict["cd"]).grid(column=1, row=i)
 
+
+
+    i += 1
+    ttk.Label(root, text="Inert mass fraction 2nd stage").grid(column=0, row=i)
+    input_dict["mf2"] = tk.StringVar(value='0.04')
+    ttk.Entry(root, textvariable=input_dict["mf2"]).grid(column=1, row=i)
+
+    i += 1
+    ttk.Label(root, text="ISP 2nd stage").grid(column=0, row=i)
+    input_dict["ISP2"] = tk.StringVar(value='296')
+    ttk.Entry(root, textvariable=input_dict["ISP2"]).grid(column=1, row=i)
+
+    #1st stage props ######################
+    i += 1
+    ttk.Label(root, text="1st stage properties", font=custom_font).grid(column=1, row=i)
+    # dV_fraction slider
+    i += 1
+    ttk.Label(root, text="dV fraction stage 1:").grid(column=0, row=i)
+    slider = ttk.Scale(root, from_=0, to=1, orient="horizontal", length=150, command=update_value)
+    slider.grid(row=i, column=1, padx=10, pady=10)
+
+    i += 1
+    input_dict["dv_split"] = tk.StringVar(value='0.5')
+    dvs = ttk.Entry(root, textvariable=input_dict["dv_split"])
+    dvs.bind("<Return>", update_slider)
+    dvs.grid(column=1, row=i)
+    update_slider()
+
+    i+=1
+    ttk.Label(root, text="Engine:").grid(column=0, row=i)
+    input_dict["engine"] = tk.StringVar()
+    engine_options = ['Prometheus']
+    ttk.Combobox(root, textvariable=input_dict["engine"], values=engine_options, state="readonly", width=17).grid(column=1, row=i)
+
+    input_dict["engine"].set("Prometheus")  # default value
+
+    i += 1
+    ttk.Label(root, text="Inert mass fraction 1st stage").grid(column=0, row=i)
+    input_dict["mf1"] = tk.StringVar(value='0.05')
+    ttk.Entry(root, textvariable=input_dict["mf1"]).grid(column=1, row=i)
+    # Boostback
+    i += 1
+    ttk.Label(root, text="Boostback:").grid(column=0, row=i)
+    input_dict["boostback"] = tk.BooleanVar(value=False)
+    ttk.Checkbutton(root, variable=input_dict["boostback"]).grid(column=1, row=i)
+
+
     # Material
     i += 1
     ttk.Label(root, text="Material Tank:").grid(column=0, row=i)
     input_dict["material_tank"] = tk.StringVar()
     material_options = list(m.keys())
-    ttk.Combobox(root, textvariable=input_dict["material_tank"], values=material_options, state="readonly").grid(column=1, row=i)
+    ttk.Combobox(root, textvariable=input_dict["material_tank"], values=material_options, state="readonly", width=17
+                 ).grid(column=1, row=i)
 
     input_dict["material_tank"].set(material_options[0])  # default value
 
@@ -189,7 +229,8 @@ if __name__ == "__main__":
     ttk.Label(root, text="Material Misc:").grid(column=0, row=i)
     input_dict["material_misc"] = tk.StringVar()
     material_options = list(m.keys())
-    ttk.Combobox(root, textvariable=input_dict["material_misc"], values=material_options, state="readonly").grid(column=1, row=i)
+    ttk.Combobox(root, textvariable=input_dict["material_misc"], values=material_options, state="readonly", width=17
+                 ).grid(column=1, row=i)
 
     input_dict["material_misc"].set(material_options[0])  # default value
 
@@ -198,7 +239,8 @@ if __name__ == "__main__":
     ttk.Label(root, text="Bulkhead:").grid(column=0, row=i)
     input_dict["bulkhead"] = tk.StringVar()
     bulkhead_options = ['shared', 'separate']
-    ttk.Combobox(root, textvariable=input_dict["bulkhead"], values=bulkhead_options, state="readonly").grid(column=1,row=i)
+    ttk.Combobox(root, textvariable=input_dict["bulkhead"], values=bulkhead_options, state="readonly", width=17
+                 ).grid(column=1,row=i)
 
     input_dict["bulkhead"].set("shared")  # default value
 
@@ -223,23 +265,6 @@ if __name__ == "__main__":
     input_dict["OF_ratio"] = tk.StringVar(value='3.5')
     ttk.Entry(root, textvariable=input_dict["OF_ratio"]).grid(column=1, row=i)
 
-    # dV_fraction slider
-    i += 1
-    ttk.Label(root, text="dV fraction stage 1:").grid(column=0, row=i)
-    slider = ttk.Scale(root, from_=0, to=1, orient="horizontal", length=200, command=update_value)
-    slider.grid(row=i, column=1, padx=10, pady=10)
-
-    # Create an input box
-    i += 1
-    #input_box = ttk.Entry(root)
-    #input_box.grid(row=i, column=1, padx=10, pady=5)
-
-
-    input_dict["dv_split"] = tk.StringVar(value='0.5')
-    dvs = ttk.Entry(root, textvariable=input_dict["dv_split"])
-    dvs.bind("<Return>", update_slider)
-    dvs.grid(column=1, row=i)
-    update_slider()
 
     # Submit button (Example action, customize as needed)
     i+=1
