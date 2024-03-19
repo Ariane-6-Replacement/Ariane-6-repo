3231d470f143d05f8f78d8df4951de7159be8c5a
Adourleijn <a.dourleijn@gmail.com>
2024-03-19 14:42:45 +0100

    Added framework function

--- python/propulsion/volume_mass_calculator.py
+++ python/propulsion/volume_mass_calculator.py
@@ -1,5 +1,5 @@
 # IMPORTS
-from inputs import engine, first_stage, propellant
+from python.propulsion.inputs import engine, first_stage, propellant as engine, first_stage, propellant
 
 
 # determines propellant mass flow rate
@@ -13,7 +13,7 @@ def calculate_mass_flow_rate(thrust, Isp):
 def get_propellant_mass_volume(thrust, burn_time, OF_ratio):
   # INPUTS
   g0 = 9.80665  # sea level gravitational parameter
-  Isp = engine.Isp  # specific impulse
+  Isp = engine.get_Isp(OF_ratio)  # specific impulse
   density_ox = propellant.density_ox  # oxidiser density for input Temperature and Pressure (calculated using thermodynamic NIST database)
   density_fuel = propellant.density_fuel  # fuel density for input Temperature and Pressure (calculated using thermodynamic NIST database)
 
@@ -42,7 +42,7 @@ def get_propellant_mass_volume(thrust, burn_time, OF_ratio):
 if __name__ == "__main__":
   thrust = first_stage.Thrust
   burn_time = first_stage.time_burn_1st
-  OF_ratio = engine.OF_ratio
+  OF_ratio = 350
   mass_ox, mass_fuel, volume_ox, volume_fuel = get_propellant_mass_volume(thrust, burn_time, OF_ratio)
 
   # return/print whatever you want
