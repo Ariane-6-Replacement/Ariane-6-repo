ca76e512424399479d748d490f8bb92787834950
mkwapien <m.t.kwapien@student.tudelft.nl>
2024-03-22 13:49:10 +0100

    Remove old inputs

--- python/structure/structure.py
+++ python/structure/structure.py
@@ -22,8 +22,8 @@ class Structure():
         tank1 = [self.outer_radius,self.pressure1,self.material,self.thrust,self.volume1,self.mass1]
         tank2 = [self.outer_radius,self.pressure2,self.material,self.thrust,self.volume2,self.mass2]
 
-        self.input = "struc in"
-        self.output = 4
+        # self.input = "struc in"
+        # self.output = 4
 
         if type == "DUAL":
             
@@ -80,22 +80,22 @@ class Structure():
 
 
 
-if __name__ == "__main__":
-    print("CALCULATING STRUCTURE")
-    test = Structure(2.7,7E5,'2219',328, 440E3, 7E5, 273, 126E3, 20E6,'2195')
-    print('DONE')
-    print('#####################OUTPUT##############')
-    print('Mass:', test.mass_total)
-    print('Height: ', test.height_total)
-    print('#############TANK FWD####################')
-    print('Tank mass:', test._tank_fwd.mass,'height: ',test._tank_fwd.height)
-    print('Cylinder mass: ',test._tank_fwd.cylinder.mass,' heigh:',test._tank_fwd.cylinder.height,' thickenss:',test._tank_fwd.cylinder.thickness)
-    print('Dome mass aft: ',test._tank_fwd.dome_aft.mass,'heigh: ',test._tank_fwd.dome_aft.height,'thickness: ',test._tank_fwd.dome_aft.thickness)
-    print('ITS 1 height:',test._ITS_fwd.height,'mass: ',test._ITS_fwd.mass )
-    print('#############TANK AFT####################')
-    print('Tank mass:', test._tank_aft.mass,'height: ',test._tank_aft.height)
-    print('Cylinder mass: ',test._tank_aft.cylinder.mass,' heigh:',test._tank_aft.cylinder.height,' thickenss:',test._tank_aft.cylinder.thickness)
-    print('Dome mass aft: ',test._tank_aft.dome_aft.mass,'heigh: ',test._tank_aft.dome_aft.height,'thickness: ',test._tank_aft.dome_aft.thickness)
-    print('ITS 2 height:',test._ITS_aft.height,'mass: ',test._ITS_aft.mass )
-    print('#############Engine Bay####################')
-    print('EB height:',test._EB.height,'mass: ',test._EB.mass )
+# if __name__ == "__main__":
+#     print("CALCULATING STRUCTURE")
+#     test = Structure(2.7,7E5,'2219',328, 440E3, 7E5, 273, 126E3, 20E6,'2195')
+#     print('DONE')
+#     print('#####################OUTPUT##############')
+#     print('Mass:', test.mass_total)
+#     print('Height: ', test.height_total)
+#     print('#############TANK FWD####################')
+#     print('Tank mass:', test._tank_fwd.mass,'height: ',test._tank_fwd.height)
+#     print('Cylinder mass: ',test._tank_fwd.cylinder.mass,' heigh:',test._tank_fwd.cylinder.height,' thickenss:',test._tank_fwd.cylinder.thickness)
+#     print('Dome mass aft: ',test._tank_fwd.dome_aft.mass,'heigh: ',test._tank_fwd.dome_aft.height,'thickness: ',test._tank_fwd.dome_aft.thickness)
+#     print('ITS 1 height:',test._ITS_fwd.height,'mass: ',test._ITS_fwd.mass )
+#     print('#############TANK AFT####################')
+#     print('Tank mass:', test._tank_aft.mass,'height: ',test._tank_aft.height)
+#     print('Cylinder mass: ',test._tank_aft.cylinder.mass,' heigh:',test._tank_aft.cylinder.height,' thickenss:',test._tank_aft.cylinder.thickness)
+#     print('Dome mass aft: ',test._tank_aft.dome_aft.mass,'heigh: ',test._tank_aft.dome_aft.height,'thickness: ',test._tank_aft.dome_aft.thickness)
+#     print('ITS 2 height:',test._ITS_aft.height,'mass: ',test._ITS_aft.mass )
+#     print('#############Engine Bay####################')
+#     print('EB height:',test._EB.height,'mass: ',test._EB.mass )
