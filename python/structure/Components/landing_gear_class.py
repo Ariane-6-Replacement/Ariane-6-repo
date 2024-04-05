324249bbbd398f8445db8e88e2b1b0dd31b1329e
mkwapien <m.t.kwapien@student.tudelft.nl>
2024-04-05 10:31:18 +0200

    Add comments

--- python/structure/Components/landing_gear_class.py
+++ python/structure/Components/landing_gear_class.py
@@ -1,4 +1,5 @@
 import numpy as np
+from python.structure.constants import Km
 class LG:
     def __init__(self,
                  outer_radius: float,
@@ -15,23 +16,21 @@ class LG:
         :param mass: in Pa
         :param material: dictionary object from materials database (databases/materials.py)
        
+        Approach used is elaborated in detailed in DSE project raport "Cryogenic H2 and O2 propellant refuelling & transfer infrastructure in space" from 2022
         """
     @property
     def Lfp(self) -> float:
+        #NOTE: Calculated using Two-Dimensional Inelastic Impact Theory
+        Lfp = 0.1 #Radius of landing gear 
+        Dh = 2.0 #Distance between the end of nozzle and the ground, from analyzing landing gear from Falcon 9 
+        H0 = self.cg + 1.8 + Dh #Distance of the CG from the ground, assumption: engine sticks out 1.8 meters from the engine bay;
+        alpha = 35 #Tip over angle for lunar landers of Apollo Missions, source: NASA 
 
-        '''H0 - height of the center of gravity with respect to the ground in m
-        alpha - turn over angle in deg
-        Lfp - landing gear radius in m
-        '''
-        Lfp = 0.1
-        Dh = 2.0 # Analyzing landing gear from Falcon 9 
-        H0 = self.cg + 1.8 + Dh
-        alpha = 35
-        I = self.mass * (self.outer_radius)**2 
+        I = self.mass * (self.outer_radius)**2 #Conservative assumption: Rocket is a solid rod 
         
         while np.sqrt(2*Lfp**2+H0**2) * (1 - np.cos(np.radians(alpha))) * 1/(1+(self.mass + (2*Lfp**2+H0**2))/I) > np.sqrt(H0**2 + Lfp**2) - H0:
             Lfp+=0.1
-        print(f'Lfp {Lfp}')
+
         return Lfp
 
 
@@ -39,38 +38,27 @@ class LG:
 
     @property
     def LG_geometry(self) -> tuple:
-        '''Lw - width of the lander
-        tau_p - primary strut angle
-        x - width beyond the LV frame
-        Lp - length of the primary strut
-        Ls - length of the secondary strut
-        drop_h - vertical stroke limit '''
-        Dh = 2.0 # Analyzing landing gear from Falcon 9 
-        x = self.Lfp - self.outer_radius
-        drop_h = 0.2
-        ys = 1.8+ drop_h + Dh
-        Ls = np.sqrt(x**2 + ys**2)
-        tau_p = 25 #deg
+        Dh = 2.0 #Distance between the end of nozzle and the ground, from analyzing landing gear from Falcon 9 
+        x = self.Lfp - self.outer_radius #Horizonatal distance between the leg and the edge of the LV
+        drop_h = 0.2 #Maximal veritcal stroke of the suspenion, assumed
+        ys = 1.8 + drop_h + Dh #Height of the attachment point of the secondary strut with the leg deplopyed, fully extended (0 stroke)
+        Ls = np.sqrt(x**2 + ys**2) #Length of the secondary strut
+        tau_p = 25 #Primary strut angle from "PRELIMINARY DESIGN OF REUSABLE LUNAR LANDER LANDING SYSTEM" by Rusty Goh Weixiong
 
-        yp = x / np.tan( np.radians( tau_p ) )
+        yp = x / np.tan( np.radians( tau_p ) ) #Height of the attachment point of the primary strut with the leg deplopyed, fully extended (0 stroke)
 
-        Lp = np.sqrt(x**2 + yp**2)
-        # print(f'Lp {Lp}, Ls {Ls}')
+        Lp = np.sqrt(x**2 + yp**2) #Length of the primary strut
+     
         return Lp, Ls
 
 
     @property
     def mass_gear(self) -> float:
-        rho= 13.91187 * 1.2
+        rho= 13.91187 * Km #The value was calulated using data provided by Almatec - a company designign landing gear for Themis Project
         mass_p = rho * self.LG_geometry[0]
         mass_s = rho * self.LG_geometry[1]
+        #Assumption: the hydraulic system + all metal components per leg weight 250 kg max; If better data is available, it is recommended;
+        #Design Decision: Tripod landing gear configuration with 4 legs
         return (mass_p + 2 * mass_s + 250) * 4
     
-# test bit 
-# if __name__ == "__main__":
-#     mass = 6258.0080096848815
 
-#     cg=  5.273065608372961
-
-#     test = LG(2.5,mass,cg)
-#     print(f'Lg.mass {test.LG_geometry}, {test.Lfp}, {test.mass_gear}')
