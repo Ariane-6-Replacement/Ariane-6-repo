cc91fa70398ef157666aa54d33675ac6433a28d4
mkwapien <m.t.kwapien@student.tudelft.nl>
2024-04-05 10:08:28 +0200

    Add other constants

--- python/structure/constants.py
+++ python/structure/constants.py
@@ -1,8 +1,8 @@
 FOSY = 1.4
 FOSU = 1.7
-FOS_ITS = 3.0
+FOS_ITS = 3.0 # seperate FoS for its to account for more discontinuitites due to riveting;
 g_0 = 9.81 # m / s^2
-Km= 1.2 #Mass factor
-
+Km= 1.2 #Mass factor, taken fron the Launch Vehicle Reader;
+Ku = 1.05 #[-], ullage + trapped propellant + PMD extension factor for volume extension;
 
 
